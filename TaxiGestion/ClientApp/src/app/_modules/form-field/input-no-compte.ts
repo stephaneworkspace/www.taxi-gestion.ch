645dcf831e75e428aac637f3dbf8fed67e2a3fc7
Bressani Stéphane <s.bressani@bluewin.ch>
2019-09-11 19:14:45 +0200

    frontend: nouve logique avec flex pour <br />

---
+++ TaxiGestion/ClientApp/src/app/_modules/form-field/input-no-compte.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'input-form-field-input-no-compte',
+    templateUrl: './input-no-compte.html'
+})
+export class FormFieldInputNoCompte {
+    
+}
