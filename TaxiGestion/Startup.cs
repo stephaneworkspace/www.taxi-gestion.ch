39f5cf38b7796216cec93495432ea621ad1383f1
stephaneworkspace <s.bressani@bluewin.ch>
2019-11-03 23:15:35 +0100

    ajout d'un commentaire

--- TaxiGestion/Startup.cs
+++ TaxiGestion/Startup.cs
@@ -40,7 +40,7 @@ namespace TaxiGestion
 
         public IConfiguration Configuration { get; }
 
-        public string _env = "prod";
+        public string _env = "prod"; // prod or dev, all theme angular frontend in dev mod
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
